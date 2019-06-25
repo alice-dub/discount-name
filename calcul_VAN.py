694fe230a51e32c8e4c41c66bd156b276013edfe
alice-dub <alice.duval99@gmail.com>
2019-06-25 20:55:36 +0200

    1st version

--- calcul_VAN.py
+++ calcul_VAN.py
@@ -1,6 +1,6 @@
 import csv
 from sympy.solvers import solve
-from sympy import Symbol, N
+from sympy import Symbol, N, real_roots, plot
 
 with open('VAN_input.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -11,10 +11,14 @@ with open('VAN_input.csv') as csv_file:
     for row in csv_reader:
         if i > -1:
             print(row)
-            expr1 = (int(row[2]) - int(row[1])) * (1 + x)**(10-i)
+            expr1 = (int(row[2]) - int(row[1])) * (1 - x)**(11-i)
             expr = expr + expr1
             print(expr)
         i += 1
 
-result = solve(expr)
-print([N(solution) for solution in result])
+result = real_roots(expr, x)
+
+print([solution.n(2) for solution in result])
+
+expr2 = expr / (1 + x)**11
+plot(expr2, (x, 0, 100), ylabel='Discount rate')
